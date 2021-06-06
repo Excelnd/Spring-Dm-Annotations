0288293624a8b31d3538e559b4a2b0133a3be7c9
Admin i <Admin i@DESKTOP-F5BCCST>
2021-06-06 22:02:59 +0530

    Random Fortune with Qualifier, injection

--- src/com/ihs/springd/RandomService.java
+++ src/com/ihs/springd/RandomService.java
@@ -1,13 +1,28 @@
 package com.ihs.springd;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomService implements FortuneService {
+	
+	// create an array of strings
+	private String[] data = {
+			"The journey is the reward",
+			"Beware of the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+	};
+	
+	// create a random number generator
+	private Random myRandom = new Random();
+
 
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
+		// pick a random string from the array
+		int index = myRandom.nextInt(data.length);
+		
 		return null;
 	}
 
