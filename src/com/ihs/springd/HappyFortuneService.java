816e0b6424542c54edf7043d7b3a7dc8a4663b68
Admin i <Admin i@DESKTOP-F5BCCST>
2021-06-07 11:48:09 +0530

    Java type AnnotationConfigApplicationContext

--- src/com/ihs/springd/HappyFortuneService.java
+++ src/com/ihs/springd/HappyFortuneService.java
@@ -9,6 +9,6 @@ public class HappyFortuneService implements FortuneService {
 	public String getFortune() {
 		
 		return "Today is your lucky day!";
-	}
+	} 
 
 }
---
+++ src/com/ihs/springd/SportConfig.java
@@ -0,0 +1,10 @@
+package com.ihs.springd;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ihs.springd")
+public class SportConfig {
+	
+}
